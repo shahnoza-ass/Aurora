d9ace84c87aa66d0c6aade675fb7ef2a52efc91b
feruza011 <feruzazayniddinova58@gmail.com>
2024-12-05 11:17:26 +0500

    Wrote basic JavaScript for a Whack-a-mole game

--- whack-a-mole.js
+++ whack-a-mole.js
@@ -1,1 +1,23 @@
+const holes = document.querySelectorAll('.hole');
+const scoreBoard = document.querySelector('.score');
+const moles = document.querySelectorAll('.mole');
+let lastHole, timeUp = false, score = 0;
+
+function randomHole(holes) {
+    const idx = Math.floor(Math.random() * holes.length);
+    const hole = holes[idx];
+    if (hole === lastHole) return randomHole(holes);
+    lastHole = hole;
+    return hole;
+}
+
+function peep() {
+    const time = Math.random() * 1000 + 200;
+    const hole = randomHole(holes);
+    hole.classList.add('up');
+    setTimeout(() => {
+        hole.classList.remove('up');
+        if (!timeUp) peep();
+    }, time);
+}
 
